4dc8756bb061b0a9de8d886613c08d95da94306f
Joseph Alaye <jamstech07@gmail.com>
2026-08-14 07:57:25 +0200

    feat(models): ajout de la gestion des roles utilisateurs (admin, artisan, visiteur)

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password'])]
+#[Fillable(['name', 'email', 'password', 'role'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
@@ -29,4 +29,7 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function artisan() { return $this->hasOne(Artisan::class); }
+    public function reservations() { return $this->hasMany(ReservationRequest::class); }
 }
